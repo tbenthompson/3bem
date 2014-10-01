e5058886f5ff9f108c37718e8ad9dfa085238701
Ben Thompson <t.ben.thompson@gmail.com>
2014-09-30 21:59:41 -0400

    AVX vectorized direct evaluation.

--- src/direct.h
+++ src/direct.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <functional>
 #include "numerics.h"
+#include <immintrin.h>
 
 typedef std::function<double (std::array<double,3>, std::array<double,3>)> Kernel;
 
@@ -28,6 +29,42 @@ inline std::vector<double> direct_n_body(std::vector<std::array<double,3>>& src_
         for (unsigned int j = 0; j < src_locs.size(); ++j) {
             out_vals[i] += kernel(obs_locs[i], src_locs[j]);
         }
+    }
+    return out_vals;
+}
+
+inline std::vector<float> vec_direct_n_body(std::array<std::vector<float>,3>& src_locs,
+                                  std::array<std::vector<float>,3>& obs_locs,
+                                  std::vector<float>& values) 
+{
+    const float factor = 1.0 / (4 * M_PI);
+    const __m256 factor_rep = _mm256_broadcast_ss(&factor);
+
+    std::vector<float> out_vals(obs_locs[0].size(), 0.0);
+#pragma omp parallel for
+    for (unsigned int i = 0; i < obs_locs[0].size(); i += 8) {
+        __m256 temp_out = _mm256_setzero_ps();
+        __m256 obs_loc_x = _mm256_loadu_ps(&obs_locs[0][i]);
+        __m256 obs_loc_y = _mm256_loadu_ps(&obs_locs[1][i]);
+        __m256 obs_loc_z = _mm256_loadu_ps(&obs_locs[2][i]);
+        for (unsigned int j = 0; j < src_locs[0].size(); j++) {
+            __m256 src_loc_x = _mm256_broadcast_ss(&src_locs[0][j]);
+            __m256 src_loc_y = _mm256_broadcast_ss(&src_locs[1][j]);
+            __m256 src_loc_z = _mm256_broadcast_ss(&src_locs[2][j]);
+            __m256 src_values = _mm256_broadcast_ss(&values[j]);
+            __m256 dx = _mm256_sub_ps(obs_loc_x, src_loc_x);
+            __m256 dy = _mm256_sub_ps(obs_loc_y, src_loc_y);
+            __m256 dz = _mm256_sub_ps(obs_loc_z, src_loc_z);
+            __m256 dx2 = _mm256_mul_ps(dx, dx);
+            __m256 dy2 = _mm256_mul_ps(dy, dy);
+            __m256 dz2 = _mm256_mul_ps(dz, dz);
+            __m256 r2 = _mm256_add_ps(_mm256_add_ps(dx2, dy2), dz2);
+            __m256 inv_r = _mm256_rsqrt_ps(r2);
+            __m256 kernel_eval = _mm256_mul_ps(inv_r, factor_rep);
+            __m256 o = _mm256_mul_ps(kernel_eval, src_values);
+            temp_out = _mm256_add_ps(o, temp_out);
+        }
+        _mm256_storeu_ps(&out_vals[i], temp_out);
     }
     return out_vals;
 }
--- test/test_evaluate.cpp
+++ test/test_evaluate.cpp
@@ -9,21 +9,58 @@ const double PI = 4.0 * std::atan(1.0);
 TEST(Direct) {
     CHECK_CLOSE(one_kernel({0, 0, 0}, {0, 0, 0}), 1.0, 1e-14);
 
-    int n = 1024 * 10;
+    int n = 8 * 1000;
     auto src = random_pts(n);
     auto obs = random_pts(n);
-    std::vector<double> values(n);
-    for (int i = 0; i < n; ++i) values[i] = 1.0;
+    std::vector<double> values(n, 1.0);
 
     Kernel k = laplace_single;
     TIC
     auto result = direct_n_body(src, obs, k, values);
     TOC("Direct N Body");
-    std::cout << "number of interaction: " << n * n << std::endl;
+    // std::cout << "number of interaction: " << n * n << std::endl;
     
 
 
     // std::vector<double> correct(n);
     // for (int i = 0; i < n; ++i) correct[i] = n;
     // CHECK_ARRAY_CLOSE(correct, result, n - 1, 1e-8);
+}
+
+TEST(FastDirect) {
+    int n = 8 * 40000;
+    bool check = n < 2000;
+    std::array<std::vector<double>,3> src =
+        {random_list(n), random_list(n), random_list(n)};
+    std::array<std::vector<double>,3> obs =
+        {random_list(n), random_list(n), random_list(n)};
+    std::array<std::vector<float>,3> srcf;
+    std::array<std::vector<float>,3> obsf;
+    std::vector<std::array<double, 3>> slow_src(n);
+    std::vector<std::array<double, 3>> slow_obs(n);
+    for (int d = 0; d < 3; d++) {
+        srcf[d].resize(n);
+        obsf[d].resize(n);
+        for (int i = 0; i < n; i++) {
+            srcf[d][i] = src[d][i];
+            obsf[d][i] = obs[d][i];
+            slow_src[i][d] = src[d][i];
+            slow_obs[i][d] = obs[d][i];
+        }
+    }
+    std::vector<float> values(n, 1.0f);
+    std::vector<double> slow_values(n, 1.0);
+    TIC
+    auto result = vec_direct_n_body(srcf, obsf, values);
+    TOC("Fast Direct N Body");
+    std::cout << "number of interaction: " << ((long)n) * ((long)n) << std::endl;
+
+    if (check) {
+        Kernel k = laplace_single;
+        auto exact = direct_n_body(slow_src, slow_obs, k, slow_values);
+        CHECK_ARRAY_CLOSE(exact, result, n, 1e-1);
+    }
+    // for (auto r: result) {
+    //     std::cout << r << std::endl;
+    // }
 }
