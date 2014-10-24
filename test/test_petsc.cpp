03539e0b95dc547068456369d6897409a53e5ee8
Ben Thompson <t.ben.thompson@gmail.com>
2014-10-23 21:46:55 -0400

    Another test of PETSc krylov solution.

--- test/test_petsc.cpp
+++ test/test_petsc.cpp
@@ -19,9 +19,29 @@ TEST(SolveSystem) {
     CHECK_ARRAY_CLOSE(res, exact, n, 1e-12);
 }
 
-// TEST(Solve2X2) {
-//     std::vector<double> rhs = {random 
-// }
+TEST(Solve2X2) {
+    std::vector<double> rhs = random_list(2);
+    std::vector<double> row1 = random_list(2);
+    std::vector<double> row2 = random_list(2);
+    std::array<std::vector<double>,2> matrix = {row1,row2};
+
+    auto res = solve_system(rhs, 1e-12, 
+        [&](std::vector<double>& x, std::vector<double>& y) {
+            for(unsigned int i = 0; i < 2; i++) {
+                y[i] = 0;
+                for(unsigned int j = 0; j < 2; j++) {
+                    y[i] += matrix[i][j] * x[j];
+                }
+            }
+        });
+
+    double denom = (matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]);
+    std::vector<double> exact = {
+        (rhs[0] * matrix[1][1] - rhs[1] * matrix[0][1]) / denom,
+        (rhs[1] * matrix[0][0] - rhs[0] * matrix[1][0]) / denom,
+    };
+    CHECK_ARRAY_CLOSE(res, exact, 2, 1e-12);
+}
 
 int main(int, char const* args[])
 {
