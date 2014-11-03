6af6d04fe457f7e167b21a46c9746760c0b9223e
Ben Thompson <t.ben.thompson@gmail.com>
2014-11-02 22:35:36 -0500

    Working on automatic differentiation!

--- test/test_bem.cpp
+++ test/test_bem.cpp
@@ -17,9 +17,9 @@ struct IntegrationProb {
         src_vals({1.0, 1.0, 1.0}),
         obs_loc({2.0, 2.0, 2.0}),
         q(tri_gauss(2)),
-        face(src_locs)
+        face(Mesh{src_locs, {{0,1,2}}}, 0)
     { }
-    std::array<Vec3<double>,3> src_locs;
+    std::vector<Vec3<double>> src_locs;
     Vec3<double> src_vals;
     Vec3<double> obs_loc;
     QuadratureRule2D q;
@@ -57,12 +57,14 @@ TEST_FIXTURE(IntegrationProb, IntegralBasis) {
 }
 
 TEST(Richardson) {
-    double result = richardson_step({0.5, 0.3, 0.2, 0.15});
+    std::vector<double> input = {0.5, 0.3, 0.2, 0.15};
+    double result = richardson_step(input);
     CHECK_CLOSE(result, 0.1, 1e-12);
 }
 
 TEST(RichardsonZeros) {
-    double result = richardson_step({0.0, 0.0, 0.0, 0.0});
+    std::vector<double> input = {0.0, 0.0, 0.0, 0.0};
+    double result = richardson_step(input);
     CHECK_CLOSE(result, 0.0, 1e-12);
 }
 
@@ -101,6 +103,16 @@ struct EvalProb {
         return eval_integral_equation(sphere, q, kernel, ne, obs_pt,
                                       obs_normal, obs_len_scale, src_strength);
     }
+    double go_row() {
+        auto row = integral_equation_vector(sphere, q, kernel, ne, obs_pt,
+                                      obs_normal, obs_len_scale);
+        double row_sum = 0.0;
+        for(std::size_t i = 0; i < row.size(); i++) {
+            row_sum += row[i] * src_strength[i];
+        }
+        return row_sum;
+    }
+
     Mesh sphere;
     QuadratureRule2D q;
     KernelFnc kernel;
@@ -114,14 +126,18 @@ struct EvalProb {
 TEST(EvalIntegralEquationSphereSurfaceArea) {
     EvalProb ep(9, 3, 2, one);
     double result = ep.go();
+    double result2 = ep.go_row();
     double exact_surf_area = 4*M_PI*9;
     CHECK_CLOSE(result, exact_surf_area, 1e-3);
+    CHECK_CLOSE(result2, exact_surf_area, 1e-3);
 }
 
 TEST(ConstantLaplace) {
     EvalProb ep(5, 3, 2, laplace_double);
     double result = ep.go();
+    double result2 = ep.go_row();
     CHECK_CLOSE(result, 1.0, 1e-3);
+    CHECK_CLOSE(result2, 1.0, 1e-3);
 }
 
 TEST(ConstantLaplaceBoundary) {
@@ -132,8 +148,18 @@ TEST(ConstantLaplaceBoundary) {
         ep.obs_pt = ep.sphere.vertices[i];
         ep.obs_normal = -normalized(ep.obs_pt);
         double result = ep.go();
+        double result2 = ep.go();
         CHECK_CLOSE(result, 1.0, 1e-2);
+        CHECK_CLOSE(result2, 1.0, 1e-2);
     }
+}
+
+TEST(MatrixRowVsEval) {
+    EvalProb ep(5, 3, 2, laplace_double);
+    double result = ep.go();
+    double result2 = ep.go_row();
+    CHECK_CLOSE(result, 1.0, 1e-3);
+    CHECK_CLOSE(result2, 1.0, 1e-3);
 }
 
 TEST(Whoa_baNANnas) {
@@ -174,17 +200,32 @@ TEST(DirectInteractOne) {
     }
     sphere = clean_mesh(sphere);
     auto q = tri_gauss(2);
-    std::vector<double> str(sphere.vertices.size(), 1.0);
+    int n_verts = sphere.vertices.size();
+    std::vector<double> str(n_verts, 1.0);
     auto res = direct_interact(sphere, sphere, q, q, one, str, 2);
+    auto matrix = interact_matrix(sphere, sphere, q, q, one, 2);
+    std::vector<double> res2(n_verts, 0.0);
+    for (int i = 0; i < n_verts; i++) {
+        for (int j = 0; j < n_verts; j++) {
+            res2[i] += matrix[i][j] * str[j]; 
+        }
+    }
     double total = 0.0;
     for (auto r: res) {
         total += r;
     }
+    double total2 = 0.0;
+    for (auto r2: res2) {
+        total2 += r2;
+    }
     double sa2 = pow(4 * M_PI, 2);
     double error = std::fabs((sa2 - total) / sa2);
+    double error2 = std::fabs((sa2 - total2) / sa2);
     CHECK_CLOSE(error, 0.0, 6.4e-3);
+    CHECK_CLOSE(error2, 0.0, 6.4e-3);
 }
 
+//TODO: Fixture for this and the next one.
 TEST(DirectInteractConstantLaplace) {
     Mesh sphere = clean_mesh(sphere_mesh({0,0,0}, 1.0));
     for (int i = 0; i < 3; i++) {
@@ -199,8 +240,6 @@ TEST(DirectInteractConstantLaplace) {
     CHECK_ARRAY_CLOSE(res0, res2, sphere.vertices.size(), 1e-2);
     CHECK_ARRAY_CLOSE(res1, res2, sphere.vertices.size(), 1e-2);
 }
-
-
 
 int main(int, char const *[])
 {
