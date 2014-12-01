78ec8f56ce75e394fd3761906fb2769a742a4051
Ben Thompson <t.ben.thompson@gmail.com>
2014-12-01 17:14:16 -0500

    Interpolate function.

--- inttest/laplace2d.cpp
+++ inttest/laplace2d.cpp
@@ -2,6 +2,9 @@
 #include "laplace.h"
 #include <iostream>
 
+Vec2<double> center = {5, 0};
+double r = 3.0;
+
 double log_u(Vec2<double> x) {
     return std::log(std::sqrt(x[0] * x[0] + x[1] * x[1]));
 }
@@ -10,29 +13,33 @@ double theta_u(Vec2<double> x) {
     return std::atan(x[1] / x[0]);
 }
 
-double log_dudn(Vec2<double> loc, Vec2<double> n) {
-    return dot(n, loc) / hypot2(loc);
-}
+struct LogDudn {
+    double operator()(Vec2<double> loc) const {
+        auto n = normalized(center - loc);
+        return dot(n, loc) / hypot2(loc);
+    }
+};
     
-double theta_dudn(Vec2<double> loc, Vec2<double> n) {
-    double x = loc[0];
-    double y = loc[1];
-    double dy = 1.0 / (x * (1 + (y * y / (x * x))));
-    double dx = (-y / x) * dy;
-    return dot(n, {dx, dy});
-}
+struct ThetaDudn {
+    double operator()(Vec2<double> loc) const {
+        auto n = normalized(center - loc);
+        double x = loc[0];
+        double y = loc[1];
+        double dy = 1.0 / (x * (1 + (y * y / (x * x))));
+        double dx = (-y / x) * dy;
+        return dot(n, {dx, dy});
+    }
+};
 
 int main() {
-    const Vec2<double> center = {5, 0};
-    double r = 3.0;
     double obs_radius = 2.9;
-    int refine_level = 10;
+    int refine_level = 7;
     int n_test_pts = 100;
     auto circle = circle_mesh(center, r).refine_repeatedly(refine_level);
     std::vector<Vec2<double>> test_pts(n_test_pts);
     for (int i = 0; i < n_test_pts; i++) {
         test_pts[i] = random_pt_sphere<2>(center, random_val() * obs_radius);
     }
-    run_laplace_test<2>(circle, test_pts, log_u, log_dudn);
-    run_laplace_test<2>(circle, test_pts, theta_u, theta_dudn);
+    dirichlet_laplace_test<2>(circle, test_pts, log_u, LogDudn());
+    dirichlet_laplace_test<2>(circle, test_pts, theta_u, ThetaDudn());
 }
---
+++ src/basis.h
@@ -0,0 +1,18 @@
+#ifndef __NWQPOISJMNLJSDROIT_BASIS_H
+#define __NWQPOISJMNLJSDROIT_BASIS_H
+#include "mesh.h"
+
+template <int dim, typename Fnc>
+std::vector<double> interpolate(const Mesh<dim>& mesh,
+                                const Fnc& fnc) {
+    int n_dofs = dim * mesh.facets.size();
+    std::vector<double> res(n_dofs);
+    for (unsigned int i = 0; i < mesh.facets.size(); i++) {
+        for (int d = 0; d < dim; d++) {
+            res[dim * i + d] = fnc(mesh.facets[i].vertices[d]);
+        }
+    }
+    return res;
+}
+
+#endif
