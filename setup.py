7250898f62aee12fa2e66899c9daeb7045c4d938
Ben Thompson <t.ben.thompson@gmail.com>
2015-05-01 01:26:14 -0400

    Removing eigen from git history

--- setup.py
+++ setup.py
@@ -1,36 +1,33 @@
 from distutils.core import setup, Extension
-from Cython.Build import cythonize
 from tools.config import include_dirs, base_cpp_flags, cpp_flag_types
-# from tools.util import files_in_dir
+from tools.util import files_in_dir
 
-# lib_srces = files_in_dir('cpp', 'cpp')
-# wrapper_srces = files_in_dir('python_wrapper', 'cpp')
-# tbempy_srces = lib_srces + wrapper_srces
-# tbempy_srces = [s + '.cpp' for s in tbempy_srces]
+lib_srces = files_in_dir('cpp', 'cpp')
+wrapper_srces = files_in_dir('python_wrapper', 'cpp')
+tbempy_srces = lib_srces + wrapper_srces
+tbempy_srces = [s + '.cpp' for s in tbempy_srces]
 includes = ['./cpp', './lib/eigen']
 cpp_flags = base_cpp_flags + cpp_flag_types['release']
-link_args = [
-    '-fopenmp', '-fPIC'
+cpp_flags += ['-Wno-unused-variable']
+libs = [
+    'boost_python'
 ]
-# tbempy = Extension(
-#     'tbempy',
-#     sources = tbempy_srces,
-#     include_dirs = includes,
-#     libraries = libs,
-#     extra_compile_args = cpp_flags,
-#     extra_link_args = link_args,
-#     language = 'c++'
-# )
-tbempy = cythonize(Extension(
-    'mesh',
-    sources = ['cython_wrapper/mesh.pyx'],
+link_args = [
+    '-fopenmp'
+]
+tbempy = Extension(
+    'tbempy',
+    sources = tbempy_srces,
     include_dirs = includes,
+    libraries = libs,
     extra_compile_args = cpp_flags,
     extra_link_args = link_args,
     language = 'c++'
-))
+)
 
 setup(
-    name = 'Name',
-    ext_modules = tbempy
+    name = 'tbempy',
+    version = '1.0',
+    description = 'This is a demo package',
+    ext_modules = [tbempy]
 )
