f594e04d984f754d3da7e165d3e45898d7d51b98
Ben Thompson <t.ben.thompson@gmail.com>
2015-05-02 02:22:55 -0400

    Boost.Numpy test

--- py/vector_to_array.py
+++ py/vector_to_array.py
@@ -1,13 +1,14 @@
 from tbempy import *
 from tbempy.TwoD import *
-import matplotlib.pyplot as plt
+import numpy as np
 
 def test_vector_to_array():
     circle = circle_mesh([0, 0], 1.0, 2)
-    for i in range(circle.facets.shape[0]):
-        f = circle.facets[i, :, :]
-        plt.plot([f[0][0], f[1][0]], [f[0][1], f[1][1]])
-    plt.show()
+    np.testing.assert_almost_equal(
+        circle.facets[3,:,:],
+        [[0.31622777, 0.9486833], [0.0, 1.0]],
+        6
+    )
 
 if __name__ == "__main__":
     test_vector_to_array()
